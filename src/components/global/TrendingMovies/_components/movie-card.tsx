d638eaa0bc8283de0e4708e27684e307e1555dd2
dewumi-thinker <oreoluwa.adewunmi@interswitchng.com>
2026-03-16 22:46:36 +0100

    cards update

--- src/components/global/TrendingMovies/_components/movie-card.tsx
+++ src/components/global/TrendingMovies/_components/movie-card.tsx
@@ -3,16 +3,33 @@ import type { MovieProps } from "../../../../types/types";
 
 export default function MovieCard({ title, year, genre, rating, image, trailer }: MovieProps) {
   return (
-    <div className='relative min-w-[180px] h-[320px] rounded-xl overflow-hidden group cursor-pointer hover:scale-125 hover:z-50 hover:min-w-[250px] hover:shadow-md hover:ring-1 hover:ring-amber-300 transition-all'>
+    <div
+      className='
+      relative
+      min-w-[180px] h-[320px]
+      rounded-md overflow-hidden
+      cursor-pointer
+      group
+      transition-all duration-300 ease-out
+      hover:scale-110 hover:-translate-y-3
+      hover:z-20
+      hover:shadow-2xl
+      hover:ring-1 hover:ring-amber-300
+      '
+    >
       {/* background image */}
-      <img src={image} className='absolute inset-0 w-full h-full object-cover' />
+      <img
+        src={image}
+        alt={title}
+        className='absolute inset-0 w-full h-full object-cover transition-transform duration-500 group-hover:scale-110'
+      />
 
       {/* gradient overlay */}
       <div className='absolute inset-0 bg-gradient-to-t from-black/80 via-black/20 to-transparent' />
 
       {/* play button */}
       {trailer && (
-        <div className='absolute inset-0 flex items-center justify-center'>
+        <div className='absolute inset-0 flex items-center justify-center z-10 bg-green-400 group-hover:opacity-100 transition-opacity duration-300'>
           <div className='bg-white/80 rounded-full p-4 backdrop-blur'>
             <Play className='w-6 h-6 text-black' />
           </div>
@@ -20,7 +37,7 @@ export default function MovieCard({ title, year, genre, rating, image, trailer }
       )}
 
       {/* bottom info */}
-      <div className='absolute bottom-4 left-4 right-4 text-white'>
+      <div className='absolute bottom-4 left-4 right-4 text-white z-10'>
         <div className='flex items-center gap-1 text-sm mb-1'>
           <Star className='w-4 h-4 text-yellow-400 fill-yellow-400' />
           <span>{rating}</span>
--- src/components/global/TrendingMovies/movie-carousel.tsx
+++ src/components/global/TrendingMovies/movie-carousel.tsx
@@ -34,7 +34,7 @@ export default function MovieCarousel() {
   return (
     <div className='w-full py-6 px-6'>
       <h1 className='text-[0b1220] text-2xl py-5'>Trending Movies</h1>
-      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 gap-6 relative'>
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-6 lg:max-w-[1200px] m-auto relative'>
         {movies.map((movie) => (
           <MovieCard key={movie.id} {...movie} />
         ))}
