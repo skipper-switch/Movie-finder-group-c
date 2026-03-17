65a1c86c73a6006e4c52e61f9f7d7c2d03ab4c23
skipper-switch <oluwatosin.adegoroye.interswitchng.com>
2026-03-17 10:34:46 +0100

    feat: moviesPage

---
+++ src/pages/Movies/Index.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+interface Video {
+  key: string;
+  type: string;
+  site: string;
+}
+
+const MoviesPage = () => {
+  const { id } = useParams();
+
+  const [video, setVideo] = useState<Video | null>(null);
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const trailer = data.results.find(
+          (v: Video) => v.type === "Trailer" && v.site === "YouTube",
+        );
+        setVideo(trailer);
+      });
+  }, [id]);
+
+  if (!video)
+    return (
+      <div className="mt-24 text-white">
+        <p>Loading...</p>
+      </div>
+    );
+
+  return (
+    <div className="mt-24 py-20 px-6 text-white">
+      {video && (
+        <iframe
+          width="100%"
+          height="400"
+          src={`https://www.youtube.com/embed/${video.key}`}
+          title="Trailer"
+          allowFullScreen
+        />
+      )}
+    </div>
+  );
+};
+
+export default MoviesPage;
